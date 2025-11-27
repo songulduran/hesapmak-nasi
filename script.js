8a5bcb6f16e1d84431aa61b50c6205a1d30f52a8
laterose <sngldrn70.1234@gmail.com>
2025-11-27 20:15:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+var ekranDegeri = "0";
+
+function ekraniGuncelle() {
+  var ekran = document.getElementById("total");
+  ekran.innerHTML = ekranDegeri;
+}
+
+function sayiEkle(sayi) {
+  if (ekranDegeri == "0") {
+    ekranDegeri = sayi;
+  } else {
+    ekranDegeri = ekranDegeri + sayi;
+  }
+  ekraniGuncelle();
+}
+
+function operatorEkle(operator) {
+  ekranDegeri = ekranDegeri + operator;
+  ekraniGuncelle();
+}
+
+function temizle() {
+  ekranDegeri = "0";
+  ekraniGuncelle();
+}
+
+function sil() {
+  if (ekranDegeri.length > 1) {
+    ekranDegeri = ekranDegeri.slice(0, -1);
+  } else {
+    ekranDegeri = "0";
+  }
+  ekraniGuncelle();
+}
+
+function hesapla() {
+  try {
+    var sonuc = eval(ekranDegeri);
+    ekranDegeri = sonuc.toString();
+    ekraniGuncelle();
+  } catch (error) {
+    ekranDegeri = "Hata";
+    ekraniGuncelle();
+  }
+}
+
+function yuzde() {
+  let sayi1 = parseFloat(document.getElementById("sayi1").value);
+  let sayi2 = parseFloat(document.getElementById("sayi2").value);
+  let sonuc;
+}
